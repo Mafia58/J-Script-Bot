9fc2f30a3f0fec94165784824ac3655ea81c219a
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-03-20 06:56:42 +0530

    Java Bot

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'Link Here',// Database Link Of MongoDB
+
+apikey:'Key Here',// Coinbase API Key
+
+secretkey:'Key ',// Coinbase 2nd Key
+
+bot_token:'Token Here',// Bot Token
+
+bot_name:'Name Here',// Bot Username Without @
+
+bot_admin:11111111,// Admin Telegram Id
+
+channelsList : ['@Cha'],// Channels List
+
+channelscheck : ['@Cha'],// Check Channels List
+
+reffer_bonus:0.01,// Refferal Bonus Amount
+
+min_wd:0.001,// Min Withdrawal Amount
+
+daily_bonus:0.01,// Daily Bonus Amount
+
+currency:'DOGE', // Bot Currency
+
+payment_channel:'@Cha'// Payment Channel Username
+
+}
